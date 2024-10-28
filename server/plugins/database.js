8a43d37a3317d3a4e58bdda207efe76b398280e3
Vitor Silva <vitorreissil@gmail.com>
2024-10-28 08:44:51 -0300

    Limpeza e conexão com o banco

---
+++ server/plugins/database.js
@@ -0,0 +1,24 @@
+// database.js
+import pkg from 'pg';
+const { Pool } = pkg;
+
+export default async function connectDatabase() {
+  const pool = new Pool({
+    user: 'postgres',
+    host: 'fretai-dev.cd608okioi2v.sa-east-1.rds.amazonaws.com',
+    database: 'postgres',
+    password: 'RvDhpqjy5g0uYTj454Do',
+    port: 5432,
+    ssl: {
+      rejectUnauthorized: false, // Ignora a verificação do certificado
+    }
+  });
+
+  try {
+    const client = await pool.connect();
+    console.log('Conexão com o banco de dados bem-sucedida!');
+    client.release(); // Liberar o cliente de volta para o pool
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error.message);
+  }
+}
