f773504d35588094031d54c1462b0c2d6e1ddaff
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2024-11-02 10:05:56 -0300

    html basic

---
+++ pages/myUsers.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <img src="../public/images/iconeTexto.png" alt="">
+        <h1>Participantes</h1>
+        <ul> 
+            <p>Joaozinho</p>
+        </ul>
+        <button>
+            Adicionar Participante
+        </button>
+    </div>
+</template>
+
---
+++ pages/registerUserDriver.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+        <img src="../public/images/iconeImage.png" alt="">
+
+        <h1> Página de Cadastro </h1>
+        
+        <input type="text" placeholder="Email do participante" >
+        
+        <button> Convidar Cliente </button>
+        <button> Voltar </button>
+
+    </div>
+</template>
