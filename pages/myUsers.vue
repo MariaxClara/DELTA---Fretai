b60bd3041df08b7d430eebb6dc2c5ed86d4cea7d
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2024-11-02 19:15:59 -0300

    css page my users

--- pages/myUsers.vue
+++ pages/myUsers.vue
@@ -1,13 +1,43 @@
 <template>
     <div>
-        <img src="../public/images/iconeTexto.png" alt="">
-        <h1>Participantes</h1>
-        <ul> 
-            <p>Joaozinho</p>
-        </ul>
-        <button>
-            Adicionar Participante
-        </button>
+        <div class="divLogo">
+            <img src="../public/images/iconeTexto.png" alt="">
+        </div>
+        <div class="divList">
+            <div class="divListTop">
+                <img class="listTop" src="../public/images/PeopleIcon.svg" alt="">
+                <h1 class="listTop">Participantes</h1>
+            </div>
+            <div class="divListItens">
+                <ul> 
+                    <div class="listItens">
+                        <img class="listImage" src="../public/images/PeopleExample.svg" alt="">
+                        <div class="listUser">
+                            <p class="listUserName">Joaozinho</p>
+                            <img class="listUserName" src="" alt="">
+                        </div>
+                    </div>
+                </ul>
+                <ul> 
+                    <div class="listItens">
+                        <img class="listImage" src="../public/images/PeopleExample.svg" alt="">
+                        <div class="listUser">
+                            <p class="listUserName">Joaozinho</p>
+                            <img class="listUserName" src="" alt="">
+                        </div>
+                    </div>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="divButton">
+            <button class="mainButton">
+                Adicionar Participante
+            </button>
+            <button class="mainButton">
+                Editar Participantes
+            </button>
+        </div>
     </div>
 </template>
 
