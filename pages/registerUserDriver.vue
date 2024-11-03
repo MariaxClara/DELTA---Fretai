229c20efe92faf3eb5ee3380bb61f31404c4097c
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2024-11-03 11:16:21 -0300

    error and succes message

--- pages/registerUserDriver.vue
+++ pages/registerUserDriver.vue
@@ -7,12 +7,22 @@
 
 
         <div class="divInput">
-            <input type="text" class="mainInput" placeholder="Email do participante" >
+            <input v-model="userEmail" type="text" class="mainInput" placeholder="Email do participante" >
+        </div>
+
+        <div class="mainDiv">
+            <div v-if="sucessInvite" class="divSucces">
+                <p class="textSucces">convite enviado com sucesso!</p>
+            </div>
+            
+            <div v-if="errorInvite" class="divError">
+                <p class="errorSucces">{{ errorMessage }}</p>
+            </div>
+
         </div>
         
-        
         <div class="divButton">
-            <button class="mainButton"> Convidar Cliente </button>
+            <button class="mainButton" @click="inviteUser()"> Convidar Cliente </button>
             <button class="mainButton">
                 <NuxtLink class="mainLink" to="/myUsers">
                     Voltar
@@ -23,3 +33,70 @@
 
     </div>
 </template>
+
+<script>
+
+    import { ref } from 'vue'
+
+    export default {
+
+        async setup() {
+
+            const errorInvite = ref(false);
+            const sucessInvite = ref(false);
+            const errorMessage = ref('');
+            const userEmails = ref([])
+            const userEmail = ref('');
+
+            //Melhorar a ReEx para mais especificidade
+            const reEmail = new RegExp(".+@.+");
+
+            userEmails.value = ["cla@uni.com", "mar@uni.com"]
+
+            function addUser(email) {
+                // Adicionar no banco
+
+                userEmails.value.push(email)
+            }
+
+            function inviteUser() {
+
+                errorInvite.value = false;
+                sucessInvite.value = false;
+
+                if (userEmail.value) {
+                    if(reEmail.test(userEmail.value)) {
+                        if(Array.from(userEmails.value).includes(userEmail.value)) {
+                            errorMessage.value = 'e-mail já cadastrado!';
+                            errorInvite.value = true;
+                        } else {
+                            sucessInvite.value = true;
+                            addUser(userEmail.value);
+                        }
+                    } else {
+                        errorMessage.value = 'e-mail inválido!';
+                        errorInvite.value = true;
+                    }
+                }   else {
+                    errorMessage.value = 'e-mail inválido!';
+                    errorInvite.value = true;
+                }
+
+                userEmail.value = '';
+
+            }
+
+            return {
+                inviteUser,
+                errorInvite,
+                sucessInvite,
+                errorMessage,
+                userEmail
+            }
+
+        }
+
+    }
+
+
+</script>
