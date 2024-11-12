446e490b871357a7d2b314b3a7b6a46f15fddf5e
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2024-11-12 13:35:16 -0300

    Desenvolvimento Front de Status de Pagamento - HTML e CSS

--- pages/myUsers.vue
+++ pages/myUsers.vue
@@ -16,7 +16,8 @@ import { NuxtLink } from '../.nuxt/components';
                     <img class="listImage" src="../public/images/PeopleExample.svg" alt="">
                     <div class="listUser">
                         <p class="listUserName">{{user.name}}</p>
-                        <img class="listUserName" src="" alt="">
+                        <img v-if="user.paid" class="listUserName" src="../public/images/HandCoinsGreen.svg" alt="">
+                        <img v-else class="listUserName" src="../public/images/HandCoinsRed.svg" alt="">
                     </div>
                 </div>
 
@@ -41,17 +42,20 @@ import { NuxtLink } from '../.nuxt/components';
     import { ref } from 'vue'
     export	default {
         async setup() {
+            let edit = ref(false)
             let users = ref([])
             users.value.push(
                 {
                     idShort: 0,
                     name: 'João da Silva',
+                    paid: true,
                 }
             )
             users.value.push(
                 {
                     idShort: 1,
                     name: 'Maria das Palmas',
+                    paid: false,
                 }
             )
 
