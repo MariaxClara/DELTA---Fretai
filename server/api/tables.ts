ead223d890b98809f22139c8ef7e82cac4bfbd67
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2024-11-26 12:28:38 -0300

    remove nuxt configs

--- server/api/tables.ts
+++
@@ -1,8 +0,0 @@
-// server/api/tables.ts
-import { getTables } from '../db/database';
-import { defineEventHandler } from 'h3';
-
-export default defineEventHandler(async () => {
-  const tables = await getTables();
-  return tables;
-});
