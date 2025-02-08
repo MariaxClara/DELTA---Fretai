a9aa436370f4749309402c8012df3bf8eedb6a83
Daniel <danielfemartins@gmail.com>
2025-02-08 11:30:40 -0300

    chat para lista de passageiros

--- src/views/MyUsers.vue
+++ src/views/MyUsers.vue
@@ -1,4 +1,3 @@
-import { NuxtLink } from '../.nuxt/components';
 <template>
     <div>
         <div class="divLogo">
@@ -11,31 +10,34 @@ import { NuxtLink } from '../.nuxt/components';
             </div>
             
             <div class="divListItens">
-
-                <div v-for="user in users" :key="user.idShort" class="listItens">
+                <div 
+                    v-for="user in users" 
+                    :key="user.idShort" 
+                    class="listItens"
+                    @click="goToChat(user.idShort)" 
+                    style="cursor: pointer;" 
+                >
                     <img class="listImage" src="/images/PeopleExample.svg" alt="">
                     <div class="listUser">
-                        <p class="listUserName">{{user.name}}</p>
+                        <p class="listUserName">{{ user.name }}</p>
                         <div v-if="!edit">
                             <img v-if="user.paid" class="listUserName" src="/images/HandCoinsGreen.svg" alt="">
                             <img v-else class="listUserName" src="/images/HandCoinsRed.svg" alt="">    
                         </div>
                         <div v-else>
-                            <img  v-if="user.paid" @click="()=> {user.paid=!user.paid; user.update = 1}" src="/images/CheckFat.svg" alt="">
-                            <img v-else @click="()=> {user.paid=!user.paid; user.update = 1}" src="/images/Selection.svg" alt="">
+                            <img v-if="user.paid" @click.stop="togglePayment(user)" src="/images/CheckFat.svg" alt="">
+                            <img v-else @click.stop="togglePayment(user)" src="/images/Selection.svg" alt="">
                         </div>
                     </div>
                 </div>
-
             </div>
-        
         </div>
         
         <div v-if="!edit" class="divButton">
-            <button  @click="()=> edit=!edit" class="mainButton">
+            <button @click="edit = !edit" class="mainButton">
                 <router-link :to="{name: 'RegisterUserDriver'}" class="mainLink">Adicionar Participante</router-link>
             </button>
-            <button @click="()=> edit=!edit" class="mainButton">
+            <button @click="edit = !edit" class="mainButton">
                 Editar Participantes
             </button>
         </div>
@@ -48,90 +50,93 @@ import { NuxtLink } from '../.nuxt/components';
     </div>
 </template>
 
-<script>
-    export	default {
-        name: 'MyUsersPage',
-    }
-</script>
-
 <script setup>
-    import { ref, onMounted } from 'vue';
-    import '../assets/css/main.css';
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import '../assets/css/main.css';
 
-    const {VITE_BASE_URL_BACKEND} = import.meta.env 
+const router = useRouter();
 
-    let edit = ref(false)
-    let users = ref([])
-    let driverId = ref(1)
-    let messageError = ref('')
+const { VITE_BASE_URL_BACKEND } = import.meta.env;
 
-    function getCookie(name) {
+let edit = ref(false);
+let users = ref([]);
+let messageError = ref('');
+
+// Função para obter o cookie do userID
+function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) {
         return decodeURIComponent(parts.pop().split(';').shift());
     }
     return null;
-    }
+}
 
-    // Obtém o email do usuário salvo no cookie
-    const id = getCookie('userID');
-    console.log('id motorista',id);
+const driverId = ref(getCookie('userID')); // Obtém o ID do motorista salvo no cookie
+console.log('id motorista:', driverId.value);
 
-    const takeUsers = async () => {
-        try {
-            console.log("Estou indo pegar meus users")
-            const response = await fetch(`${VITE_BASE_URL_BACKEND}/driverUsers/${id}`, {
-                method: 'GET',
-            })
-            const data = await response.json();
-            let passageiros = []
-            for (let i in data.body) {
-                let passageiro = data.body[i]
-                passageiros.push({
-                    idShort: i,
-                    name: passageiro.passageiro_nome,
-                    paid: passageiro.passageiro_pagamento,
-                    email: passageiro.passageiro_email,
-                    update: 0,
-                })
-            }
-            users.value = passageiros
-        } catch (error) {
-            console.log('Não consegui pegar os passageiros:')
-            console.log(error)
+// Função para buscar usuários
+const takeUsers = async () => {
+    try {
+        console.log("Estou indo pegar meus users");
+        const response = await fetch(`${VITE_BASE_URL_BACKEND}/driverUsers/${driverId.value}`, {
+            method: 'GET',
+        });
+        const data = await response.json();
+        let passageiros = [];
+        for (let i in data.body) {
+            let passageiro = data.body[i];
+            passageiros.push({
+                idShort: passageiro.passageiro_id,
+                name: passageiro.passageiro_nome,
+                paid: passageiro.passageiro_pagamento,
+                email: passageiro.passageiro_email,
+                update: 0,
+            });
         }
-
+        users.value = passageiros;
+    } catch (error) {
+        console.log('Não consegui pegar os passageiros:', error);
     }
-    async function updateUsers() {
-        edit.value=!edit.value
-        try {
-            for (let i in users.value) {
-                let user = users.value[i]
-                try {
-                    const response = await fetch(`${VITE_BASE_URL_BACKEND}/updateUserPay`, {
-                        method: 'POST',
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({
-                            email: user.email,
-                            paid: Number(user.paid)
-                        })
+};
+
+// Função para atualizar usuários
+const updateUsers = async () => {
+    edit.value = !edit.value;
+    try {
+        for (let user of users.value) {
+            try {
+                await fetch(`${VITE_BASE_URL_BACKEND}/updateUserPay`, {
+                    method: 'POST',
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        email: user.email,
+                        paid: Number(user.paid)
                     })
-                    const data = await response.json();
-                } catch (error) {
-                    messageError.value = 'Parece que nosso servidor está em manutenção, não foi possível salvar as modificações!'
-                }
+                });
+            } catch (error) {
+                messageError.value = 'Erro ao salvar as modificações. O servidor pode estar fora do ar.';
             }
-        } catch (error) {
-            messageError.value = 'Parece que nosso servidor está em manutenção, não foi possível salvar as modificações!'
-            console.log(error)
-            console.log(messageError)
         }
+    } catch (error) {
+        messageError.value = 'Erro ao salvar as modificações.';
+        console.log(error);
     }
+};
 
-    onMounted(() => {
-        takeUsers()
-    })
+// Função para ir para a página de chat com o passageiro escolhido
+const goToChat = (passageiroId) => {
+    router.push(`/chat/${driverId.value}/${passageiroId}`);
+};
 
+// Função para alternar o pagamento sem clicar no usuário
+const togglePayment = (user) => {
+    user.paid = !user.paid;
+    user.update = 1;
+};
+
+onMounted(() => {
+    takeUsers();
+});
 </script>
-
